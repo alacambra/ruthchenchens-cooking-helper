5a7e4a21a92b9e5f24b400ed1e636e8d5479ffd2
alacambra <alacambra@gmail.com>
2014-08-26 01:34:51 +0200

    use mysql

--- cookinghelper/cookinghelper/settings.tpl.py
+++ cookinghelper/cookinghelper/settings.tpl.py
@@ -57,9 +57,17 @@ WSGI_APPLICATION = 'cookinghelper.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rutchenchecn_cooking_helper',
+        'USER': 'USER',
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
