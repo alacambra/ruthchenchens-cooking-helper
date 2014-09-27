fe3225ca62ea317bb78464a51fd1d996378807ca
alacambra <alacambra@gmail.com>
2014-09-28 00:16:17 +0200

    set unique model fields

--- cookinghelper/recipe_indexer/models.py
+++ cookinghelper/recipe_indexer/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 class Book(models.Model):
     isbn = models.CharField(max_length=17, unique=True)
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=254)
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=254, unique=True)
 
 
 class RecipesCategory(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=254, unique=True)
 
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=254, unique=True)
     book = models.ForeignKey(Book)
     rating = models.IntegerField(default=0)
     page = models.IntegerField(default=-1)
