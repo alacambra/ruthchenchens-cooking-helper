da2384e30bf83230a90f363e8f8d0599f06127ee
alacambra <alacambra@gmail.com>
2014-09-27 19:13:37 +0200

    testing urls and tpls

---
+++ cookinghelper/recipe_indexer/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'alacambra'
+from django.conf.urls import patterns, url
+import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+)
--- cookinghelper/recipe_indexer/views.py
+++ cookinghelper/recipe_indexer/views.py
@@ -1,3 +1,11 @@
+from libxml2mod import name
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import *
+
 
 # Create your views here.
+def index(request):
+    ingredients = Ingredient.objects.get(name="chicoree")
+    context = {'ingredients': [ingredients]}
+    return render(request, 'recipe_indexer/index.html', context)
