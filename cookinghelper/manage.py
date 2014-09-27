22443094a4260ddac9dd79682d2a3ca390963408
alacambra <alacambra@gmail.com>
2014-09-27 19:29:29 +0200

    migration bug corrected

--- cookinghelper/manage.py
+++ cookinghelper/manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+import django
+django.setup()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cookinghelper.settings")
