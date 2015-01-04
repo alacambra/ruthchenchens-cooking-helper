e6667e10409e22dd79cdfe2452d05616a684168b
alacambra <alacambra@gmail.com>
2015-01-05 00:48:52 +0100

    Search functions added

---
+++ javaEE/app/src/main/java/alacambra/cookinghelper/search/SearchRecipeFacade.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alacambra.cookinghelper.search;
+
+import alacambra.cookinghelper.beans.AbstractFacade;
+import alacambra.cookinghelper.entities.Book;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author alacambra
+ */
+@Stateless
+public class SearchRecipeFacade extends AbstractFacade<Book> {
+    @PersistenceContext(unitName = "Cookinghelper")
+    private EntityManager em;
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SearchRecipeFacade() {
+        super(Book.class);
+    }
+    
+}
