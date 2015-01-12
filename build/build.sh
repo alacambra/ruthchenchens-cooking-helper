37c5971984c8b02360b1af4f68c45a07106e5f7d
alacambra <alacambra@gmail.com>
2015-01-13 00:11:04 +0100

    tryoing scripts configs

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+mkdir db
 sudo docker build --tag=cookinghelper /home/alacambra/cookinghelper/
 sudo docker kill cookinghelper
 sudo docker kill nginx-hub
 sudo docker rm cookinghelper
 sudo docker rm nginx-hub
-sudo docker run --name cookinghelper -itd cookinghelper
+
+sudo docker run --name cookinghelper \
+     -itd cookinghelper \
+#    -v /home/alacambra/cookinghelper/db:/opt/jboss/wildfly/db/ \
+
+
 sudo docker run -d -p 80:80 \
   --name nginx-hub --link nginx-www:www \
   --link cookinghelper:cookinghelper \
