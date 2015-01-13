0fe276afcb08747a588e0a13a15f24bfe955777f
alacambra <alacambra@gmail.com>
2015-01-13 22:38:23 +0100

    with selective persitence xml

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+cp ../src/main/resources/META-INF/persistence.prod.xml ../src/main/resources/META-INF/persistence.xml
 cd ..
 mvn clean package
 cd build
+cp ../src/main/resources/META-INF/persistence.dev.xml ../src/main/resources/META-INF/persistence.xml
 scp ../target/cookinghelper.war cookinghelper.h2.db build.sh Dockerfile standalone.xml lacambra.de:/home/alacambra/cookinghelper
 ssh lacambra.de cd /home/alacambra/cookinghelper
 ssh -t lacambra.de sudo /home/alacambra/cookinghelper/build.sh
