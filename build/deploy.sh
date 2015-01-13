0ff2566c82c32b5c34fa3b0295b990ec3aae7b1e
alacambra <alacambra@gmail.com>
2015-01-13 23:21:14 +0100

    minimal change for changing persitence.xml on deployment

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+cp ../src/main/resources/META-INF/persistence.xml ../src/main/resources/META-INF/persistence.tmp.xml
 cp ../src/main/resources/META-INF/persistence.prod.xml ../src/main/resources/META-INF/persistence.xml
 cd ..
 mvn clean package
 cd build
-cp ../src/main/resources/META-INF/persistence.dev.xml ../src/main/resources/META-INF/persistence.xml
+cp ../src/main/resources/META-INF/persistence.tmp.xml ../src/main/resources/META-INF/persistence.xml
 scp ../target/cookinghelper.war cookinghelper.h2.db build.sh Dockerfile standalone.xml lacambra.de:/home/alacambra/cookinghelper
 ssh lacambra.de cd /home/alacambra/cookinghelper
 ssh -t lacambra.de sudo /home/alacambra/cookinghelper/build.sh
