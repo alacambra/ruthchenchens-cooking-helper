9fe47c5116076332f6f746b33e30570d2caf8841
alacambra <alacambra@gmail.com>
2015-01-20 00:07:22 +0100

    not search implemented

--- src/main/java/alacambra/cookinghelper/search/SearchRecipeFacade.java
+++ src/main/java/alacambra/cookinghelper/search/SearchRecipeFacade.java
@@ -17,6 +17,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.*;
 import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -110,50 +112,45 @@ public class SearchRecipeFacade extends AbstractFacade<Recipe> {
 
 
         CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery recipesWithoutIngredientQuery;
 
-        CriteriaQuery cq;
         if(count){
-            cq = cb.createQuery(Long.class);
+            recipesWithoutIngredientQuery = cb.createQuery(Long.class);
         }else{
-            cq = cb.createQuery(Recipe.class);
+            recipesWithoutIngredientQuery = cb.createQuery(Recipe.class);
         }
 
-        Subquery<Recipe> recipesWith = cq.subquery(Recipe.class);
-        cb.createQuery(Recipe.class);
-        Root<Recipe> recipes = cq.from(Recipe.class);
-        Root<Recipe> fromRecipe = recipesWith.from(Recipe.class);
-        Path<Object> path = fromRecipe.get("id");
+        Subquery<Long> recipesWithIngredient = recipesWithoutIngredientQuery.subquery(Long.class);
+        Root<Recipe> recipeWithIngredientRoot = recipesWithIngredient.from(Recipe.class);
+        Join<Recipe, Ingredient> recipeIngredient = recipeWithIngredientRoot.join(recipeMetamodel.getSet("ingredients", Ingredient.class));
 
-        Set<Predicate> subPredicates = new HashSet<>();
-        Predicate[] ingNot = new Predicate[ingrs.length];
+        Predicate[] predicates = new Predicate[ingrs.length];
 
-        if(!criteriaIsEmpty(ingrs)) {
-            Join<Recipe, Ingredient> ing = recipes.join(recipeMetamodel.getSet("ingredients", Ingredient.class));
-            for (int i = 0; i<ingNot.length && !ingrs[i].equals("") ;i++){
-                ingNot[i] = cb.like((Expression<String>) ing.get(ingredientMetamodel.getSingularAttribute("name")), ingrs[i].trim());
-                logger.info("NOT:ING:" + ingrs[i].trim());
-            }
-            subPredicates.add(cb.or(ingNot));
+        for(int i = 0; i < ingrs.length; i++){
+            predicates[i]=cb.like(
+                    (Expression<String>) recipeIngredient.get(ingredientMetamodel.getSingularAttribute("name")),
+                    ingrs[i]
+            );
         }
 
-        recipesWith.select(recipes);
-        Predicate[] subPredicatesArr = new Predicate[subPredicates.size()];
+        recipesWithIngredient.select(recipeWithIngredientRoot.<Long>get("id")).distinct(true)
+                .where(
+                        cb.or(predicates)
+                )
+                .groupBy(recipeWithIngredientRoot.get("id"));
 
-        int j = 0;
-        for(Predicate p : subPredicates){
-            subPredicatesArr[j] = p;
-            j++;
-        }
-        cq.select(recipes).where(subPredicatesArr);
-        Predicate p = fromRecipe.get(recipeMetamodel.getSingularAttribute("id", Integer.class)).in(recipesWith).not();
+        Root<Recipe> recipesWithoutIngredientRoot = recipesWithoutIngredientQuery.from(Recipe.class);
 
         if(count){
-            cq.select(cb.countDistinct(recipes)).where(cb.not(p));
-        } else {
-            cq.select(recipes).where(p);
+            recipesWithoutIngredientQuery.select(cb.count(recipesWithoutIngredientRoot)).distinct(true);
+        }else{
+            recipesWithoutIngredientQuery.select(recipesWithoutIngredientRoot).distinct(true);
         }
 
-        return cq;
+        recipesWithoutIngredientQuery.where(
+                recipesWithoutIngredientRoot.get("id").in(recipesWithIngredient).not());
+
+        return recipesWithoutIngredientQuery;
     }
 
     private Set<Predicate> getOrPredicates(
