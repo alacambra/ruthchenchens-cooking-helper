702afe6b049a876c3527671db767f2f2a9c06217
alacambra <alacambra@gmail.com>
2015-02-21 11:54:58 +0100

    mysql database used

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,10 +2,13 @@
 
 BUILD_PATH=/home/alacambra/cookinghelper/build
 
+envsubst '${COOKINGHELPER_DB_USR}:${COOKINGHELPER_DB_PSW}' < "standalone-tpl.xml" > "standalone.xml"
 cp ../src/main/resources/META-INF/persistence.prod.xml ../src/main/resources/META-INF/persistence.xml
 cd ..
 mvn clean package
 cd build
-scp ../target/cookinghelper.war cookinghelper.h2.db build.sh Dockerfile standalone.xml lacambra.de:${BUILD_PATH}
+#scp mysql-connector-java-5.1.34-bin.jar
+scp ../target/cookinghelper.war build.sh Dockerfile standalone.xml lacambra.de:${BUILD_PATH}
 ssh lacambra.de cd ${BUILD_PATH}
 ssh -t lacambra.de sudo ${BUILD_PATH}/build.sh
+rm standalone.xml
