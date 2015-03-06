15c46652e5012e75927b58b251cc3abc9764d386
alacambra <alacambra@gmail.com>
2015-03-06 08:41:38 +0100

    use of envsubst tu configure wf users

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,6 +3,8 @@
 BUILD_PATH=/home/alacambra/cookinghelper/build
 
 envsubst '${COOKINGHELPER_DB_USR}:${COOKINGHELPER_DB_PSW}' < "standalone-tpl.xml" > "standalone.xml"
+envsubst '${COOKINGHELPER_WF_USR}:${COOKINGHELPER_WF_PSW}' < "Dockerfile-tpl" > "Dockerfile"
+
 cp ../src/main/resources/META-INF/persistence.prod.xml ../src/main/resources/META-INF/persistence.xml
 cd ..
 mvn clean package
